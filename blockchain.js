fbfd26538b63d91865c5d95e3509630686465a1d
Miguel Rivas Perez <mrivasperez@icloud.com>
2021-02-13 13:51:33 -0800

    added blockchain validation

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,35 @@ class Blockchain {
       data, // pass in new data
     });
     this.chain.push(newBlock); // push the new block to the chain
+  }
+
+  static isValidChain(chain) {
+    // MAKE SURE CONTAINS GENESIS
+    //stringify chain and compare it to the stringified genesis block
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    // HASH VALIDATION
+    for (let i = 1; i < chain.length; i++) {
+      // MAKE SURE ALL BLOCKS REFERENCE PREVIOUS BLOCKS HASHES CORRECTLY
+      const block = chain[i];
+      // save the actual last hash for previous block
+      const actualLastHash = chain[i - 1].hash;
+      // pull variables of current block
+      const { timestamp, lastHash, hash, data } = block;
+      //check if last hash equals the hash of the previous block
+      if (lastHash !== actualLastHash) return false;
+
+      // CHECK IF CURRENT HASH IS VALID
+      // generate a new hash using current fields
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+      // if the new hash is not the same as the expect hash, then it is an invalid hash
+      if (hash !== validatedHash) return false;
+    }
+
+    // PASSED ALL TESTS!
+    return true;
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
+  let blockchain = new Blockchain();
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
 
   test("contains a `chain` Array", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -17,5 +21,50 @@ describe("Blockchain", () => {
     blockchain.addBlock({ data: newData });
     // make sure that the new block includes the new data
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  // Chain validator
+  describe("isValidChain()", () => {
+    describe("when the chain does not start with the genesis block", () => {
+      beforeEach(() => {
+        blockchain = new Blockchain();
+      });
+
+      test("returns false", () => {
+        blockchain.chain[0] = { data: "fake-genesis" };
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("when the chain does start with the genesis block and has multiple blocks", () => {
+      describe("a last hash reference has changed", () => {
+        beforeEach(() => {
+          blockchain.addBlock({ data: "Bears" });
+          blockchain.addBlock({ data: "Beets" });
+          blockchain.addBlock({ data: "Berries" });
+        });
+
+        test("returns false", () => {
+          blockchain.chain[2].lastHash = "broken-lastHash";
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+
+        describe("the chain contains a block with an invalid field", () => {
+          test("returns false", () => {
+            //modify chain field
+            blockchain.chain[2].data = "some-bad-and-evil-data";
+
+            expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+          });
+        });
+
+        describe("the chain does not contain any invalid blocks", () => {
+          test("returns true", () => {
+            expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+          });
+        });
+      });
+    });
   });
 });
